d51c9d07af71dd90922ad90f5e9dea0556a5af7a
online guru web <44157539+onlineguruweb@users.noreply.github.com>
2022-09-20 17:12:36 +0530

    mongoose connect

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ app.use(express.json());
 
 console.log('testing',process.env.MONGO_URL)
 
-// app.use(process.env.MONGO_URL,{})
-// .then(()=>{
-//     console.log("mongoose connected")
-// })
-// .catch((e)=>{
-//     console.log(e);
-// })
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then(() => {
+      console.log('connected to database')
+    })
+    .catch((err) => {
+      console.log(err)
+    }) 
 
 app.use("/", home);
 // Routes
